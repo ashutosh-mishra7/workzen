93f713c828db9cd3504f26762eef67d38e8006e7
ashutosh-mishra7 <mishraashutosh9199@gmail.com>
2026-02-25 22:40:44 +0530

    fixed mongoose v9 connection

--- config/db.js
+++ config/db.js
@@ -4,13 +4,10 @@ const connectDB = async () => {
   try {
 
     if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI not defined in environment variables");
+      throw new Error("MONGO_URI not defined");
     }
 
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
 
@@ -18,8 +15,7 @@ const connectDB = async () => {
 
     console.error("❌ MongoDB Connection Error:", err.message);
 
-    // DO NOT crash instantly in production
-    setTimeout(() => process.exit(1), 5000);
+    process.exit(1);
   }
 };
 
